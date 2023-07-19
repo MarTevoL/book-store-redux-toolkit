a77d23a29593dd87d30b6842bdedaf94ec4b7fa8
Truong Le <leminhtruong.vt@gmail.com>
2023-07-19 20:32:28 +0700

    Refractor the project using Redux Toolkit

--- src/pages/ReadingPage.js
+++ src/pages/ReadingPage.js
@@ -1,74 +1,73 @@
 import React, { useState, useEffect } from "react";
-import { Container, Button, Box, Card, Stack, CardMedia, CardActionArea, Typography, CardContent } from "@mui/material";
+import {
+  Container,
+  Button,
+  Box,
+  Card,
+  Stack,
+  CardMedia,
+  CardActionArea,
+  Typography,
+  CardContent,
+} from "@mui/material";
 import { ClipLoader } from "react-spinners";
 import { useNavigate } from "react-router-dom";
-import { toast } from "react-toastify";
-import api from "../apiService";
+import { useDispatch, useSelector } from "react-redux";
+import { getReadingList, removeBook } from "../components/book/bookSlice.js";
 
 const BACKEND_API = process.env.REACT_APP_BACKEND_API;
 
 const ReadingPage = () => {
-  const [books, setBooks] = useState([]);
-  const [loading, setLoading] = useState(false);
-  const [removedBookId, setRemovedBookId] = useState("");
-  const navigate = useNavigate()
+  const readingList = useSelector((state) => state.book.readingList);
+  const status = useSelector((state) => state.book.status);
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const [removeBookId, setRemoveBookId] = useState("");
 
   const handleClickBook = (bookId) => {
     navigate(`/books/${bookId}`);
   };
 
-  const removeBook = (bookId) => {
-    setRemovedBookId(bookId);
+  const setRemoveBook = (bookId) => {
+    setRemoveBookId(bookId);
   };
 
   useEffect(() => {
-    if (removedBookId) return;
-    const fetchData = async () => {
-      setLoading(true);
-      try {
-        const res = await api.get(`/favorites`);
-        setBooks(res.data);
-      } catch (error) {
-        toast(error.message);
-      }
-      setLoading(false);
-    };
-    fetchData();
-  }, [removedBookId]);
+    if (removeBookId) return;
+    dispatch(getReadingList());
+  }, [removeBookId, dispatch]);
 
   useEffect(() => {
-    if (!removedBookId) return;
-    const fetchData = async () => {
-      setLoading(true);
-      try {
-        await api.delete(`/favorites/${removedBookId}`);
-        toast.success("The book has been removed");
-        setRemovedBookId("");
-      } catch (error) {
-        toast(error.message);
-      }
-      setLoading(false);
-    };
-    fetchData();
-  }, [removedBookId]);
+    if (!removeBookId) return;
+    dispatch(removeBook(removeBookId));
+    setRemoveBookId("");
+  }, [removeBookId, dispatch]);
 
   return (
     <Container>
-      <Typography variant="h3" sx={{ textAlign: "center" }} m={3}>Book Store</Typography>
-      {loading ? (
-        <Box sx={{ textAlign: "center", color: "primary.main" }} >
+      <Typography variant="h3" sx={{ textAlign: "center" }} m={3}>
+        Book Store
+      </Typography>
+      {status ? (
+        <Box sx={{ textAlign: "center", color: "primary.main" }}>
           <ClipLoader color="inherit" size={150} loading={true} />
         </Box>
       ) : (
-        <Stack direction="row" spacing={2} justifyContent="space-around" flexWrap={"wrap"}>
-          {books.map((book) => (
+        <Stack
+          direction="row"
+          spacing={2}
+          justifyContent="space-around"
+          flexWrap={"wrap"}
+        >
+          {readingList.map((book) => (
             <Card
               key={book.id}
               sx={{
                 width: "12rem",
                 height: "27rem",
                 marginBottom: "2rem",
-              }}>
+              }}
+            >
               <CardActionArea>
                 <CardMedia
                   component="img"
@@ -76,32 +75,35 @@ const ReadingPage = () => {
                   alt={`${book.title}`}
                   onClick={() => handleClickBook(book.id)}
                 />
-                <CardContent>
+                <CardContent component="span">
                   <Typography gutterBottom variant="h5" component="div">
                     {`${book.title}`}
                   </Typography>
                   <Typography gutterBottom variant="body1" component="div">
                     {`${book.author}`}
                   </Typography>
-                  <Button
+                  {/* <Button
                     sx={{
-                      position: "absolute", top: "5px", right: "5px",
-                      backgroundColor: "secondary.light", color: "secondary.contrastText",
-                      padding: "0", minWidth: "1.5rem"
+                      position: "absolute",
+                      top: "5px",
+                      right: "5px",
+                      backgroundColor: "secondary.light",
+                      color: "secondary.contrastText",
+                      padding: "0",
+                      minWidth: "1.5rem",
                     }}
                     size="small"
-                    onClick={() => removeBook(book.id)}
+                    onClick={() => setRemoveBook(book.id)}
                   >
                     &times;
-                  </Button>
+                  </Button> */}
                 </CardContent>
-
               </CardActionArea>
             </Card>
           ))}
         </Stack>
       )}
-    </Container >
+    </Container>
   );
 };
 
